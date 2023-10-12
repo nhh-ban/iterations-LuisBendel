cf8ea93fc2e9a7c39822a85ee72f64d4bf3f46f5
Luis Bendel <luis.rb3001@gmail.com>
2023-10-12 13:15:07 +0200

    commented data_tests.r

--- functions/data_tests.r
+++ functions/data_tests.r
@@ -5,11 +5,13 @@
 # All tests are packed in a function test_stations_metadata that apples
 # all the aforementioned tests
 
+# Test if the column names from the dataframe that is given as an input
+# are the columns that we want to have
 test_stations_metadata_colnames <-
   function(df) {
-    
+    # this is the columnnames that we want
     expected_colnames <- c("id", "name", "latestData", "lat", "lon")
-    
+    #all 5 expected columns must be in the dataframe to pass the test
     if (all(colnames(df) == expected_colnames) == TRUE) {
       print("PASS: Data has the correct columns")
     } else{
@@ -17,21 +19,27 @@ test_stations_metadata_colnames <-
     }
   }
 
+# We expect the number of rows for the given dataframe to be between 5000 and 10000
+# Test is only passed if this is true for the input dataframe
 test_stations_metadata_nrows <-
   function(df) {
-    
+    # expected boundaries for row numbers
     min_expected_rows <- 5000
     max_expected_rows <- 10000
-    
+    # test passed if nrows between the boundaries
     if (nrow(df) > min_expected_rows & nrow(df) < max_expected_rows) {
       print("PASS: Data has a reasonable number of rows")
+    # if lower than lower bound, then failed with suspiciously few rows  
     } else if (nrow(df) <= min_expected_rows) {
       print("FAIL: Data has suspiciously few rows")
+    # otherwise failed with suspiciously many rows
     } else {
       print("FAIL: Data has suspiciously many rows")
     }
   }
 
+# Checking if the function returns the dataframe with the correct column types
+# all column types must be equal to the expected ones to pass the test
 test_stations_metadata_coltypes <-
   function(df) {
     expected_coltypes <-
@@ -44,7 +52,12 @@ test_stations_metadata_coltypes <-
       print("FAIL: Columns do not have the correct specification")
     }
   }
-  
+
+# check how many values in every column (together) are missing (NA)
+# we expect to have at most 199 missing values in all columns together
+# test if failed if there are more missing values because then we can assume
+# that something is wrong with the function that outputs the dataframe
+# or something is wrong with the input dataframe or both
 test_stations_metadata_nmissing <-
   function(df) {
     max_miss_vals <- 200
@@ -56,6 +69,7 @@ test_stations_metadata_nmissing <-
     }
   }
 
+# Test if the time zone attribute of the latestData Datetime-Column is UTC
 test_stations_metadata_latestdata_timezone <-
   function(df) {
     
@@ -66,7 +80,8 @@ test_stations_metadata_latestdata_timezone <-
     }
   }
 
-
+# Running all tests at once using a single function that calls
+# all other functions
 test_stations_metadata <- 
   function(df){
     test_stations_metadata_colnames(df)
